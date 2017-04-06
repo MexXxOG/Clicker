81f335dfefa0246445cd77a6baba3e632bc347fa
Michal <csstrzelec@gmail.com>
2017-04-06 21:13:30 +0200

    Pierwsza wersja gry
    Dodanie podstawowych plików

---
+++ Clicker Game/Assets/Scripts/Click.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	public UnityEngine.UI.Text MoneyLicz;
+	public UnityEngine.UI.Text gpc;
+	public float gold = 0.00f;
+	public int goldperclick = 1;
+
+	void Update(){
+		MoneyLicz.text = "Kasa: " + gold;
+		gpc.text = "Atak: " + goldperclick;
+	}
+
+	public void Clicked(){
+		gold += goldperclick;
+	}
+
+}
